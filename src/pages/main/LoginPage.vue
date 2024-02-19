c3dcc95674c494e7a86853d200b4d8282813c1be
GyeongNam <jo_gn97@naver.com>
2024-02-19 17:42:59 +0900

    save: 이메일 회원가입 완료

--- src/pages/main/LoginPage.vue
+++ src/pages/main/LoginPage.vue
@@ -17,12 +17,12 @@
     </q-item>
     <q-item >
       <q-item-section>
-        <q-btn class="bg-red-7 text-white" flat> <q-icon class="q-mr-sm" size="1em" name="fa-brands fa-google"/> Google Login </q-btn>
+        <q-btn class="bg-red-7 text-white" flat> <q-icon class="q-mr-sm" size="1em" name="fa-brands fa-google" @click="google"/> Google Login </q-btn>
       </q-item-section>
     </q-item>
     <q-item >
       <q-item-section>
-        <q-btn class="bg-black text-white" flat><q-icon class="q-mr-sm" size="1em" name="fa-brands fa-github"/> Github Login  </q-btn>
+        <q-btn class="bg-black text-white" flat><q-icon class="q-mr-sm" size="1em" name="fa-brands fa-github" @click="github"/> Github Login  </q-btn>
       </q-item-section>
     </q-item>
     <q-item >
@@ -34,9 +34,64 @@
 </template>
 
 <script>
+import axios from "axios";
+import {useQuasar} from "quasar";
+import { ref } from 'vue';
 export default {
-  name: "LoginPage",
+  name: "LoginPage"
+  ,
+  setup() {
+    const $q = useQuasar();
 
+    function triggerOngoing() {
+      $q.notify({
+        type: 'positive',
+        message: '로그인되었습니다.',
+        timeout: 1000
+      });
+    }
+
+    function triggerNegative(message) {
+      $q.notify({
+        type: 'warning',
+        position: 'top',
+        message: message
+      });
+    }
+
+    function github(){
+      console.table('깃허브야')
+      axios.get('http://localhost:8080/oauth2/authorization/github')
+        .then(response => {
+          console.table(response)
+          triggerOngoing();
+          // this.$router.push('/');
+        })
+        .catch(error => {
+          triggerNegative(error.response.data.message);
+        });
+    }
+    function google(){
+      console.table('구글이야')
+      axios.get('http://localhost:8080/oauth2/authorization/google')
+        .then(response => {
+          console.table(response)
+          triggerOngoing();
+          // this.$router.push('/');
+        })
+        .catch(error => {
+          triggerNegative(error.response.data.message);
+        });
+    }
+
+    return{
+      triggerOngoing,
+      triggerNegative,
+      google,
+      github
+    }
+  }
+  ,
   methods:{
 
   }
