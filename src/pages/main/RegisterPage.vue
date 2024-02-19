fa05566ee7d3ac94a82056ae7237267071341901
GyeongNam <jo_gn97@naver.com>
2024-02-19 17:03:47 +0900

    feat: 회원가입
    외부 플러그인 notify 를 사용해 경고 창 띄우기

--- src/pages/main/RegisterPage.vue
+++ src/pages/main/RegisterPage.vue
@@ -15,6 +15,7 @@
             label="이메일"
             label-color="orange"
             class="q-mb-sm"
+            @keyup="validateEmail"
           />
           </div>
           <q-input
@@ -22,8 +23,8 @@
             v-model="name"
             label="이름"
             label-color="orange"
-            color="white"
             class="q-mb-sm"
+            @keyup="validateName"
           />
           <q-input
             dark
@@ -32,6 +33,7 @@
             label="닉네임"
             label-color="orange"
             class="q-mb-sm"
+            @keyup="validateNickname"
           />
           <q-input
             dark
@@ -41,6 +43,7 @@
             label="비밀번호"
             label-color="orange"
             class="q-mb-sm"
+            @keyup="validatePassword"
           />
           <q-input
             dark
@@ -50,8 +53,9 @@
             label="비밀번호 확인"
             label-color="orange"
             class="q-mb-sm"
+            @keyup="validatePasswordCheck"
           />
-
+          <div v-if="errorMessage" style="color: orange;">{{ errorMessage }}</div>
           <div class="q-mt-lg">
             <q-btn label="회원가입" type="submit" @click="register" class="text-black bg-white" style="opacity: 0.8" />
           </div>
@@ -64,8 +68,29 @@
 
 <script>
 import axios from 'axios';
-
+import { useQuasar } from 'quasar';
 export default {
+  setup() {
+    const $q = useQuasar();
+    function triggerOngoing () {
+      $q.notify({
+        type: 'positive',
+        message: '회원가입 했습니다. 로그인해주세요.',
+        timeout: 1000
+      });
+    }
+    function triggerNegative(message) {
+      $q.notify({
+        type: 'warning',
+        position: 'top',
+        message: message
+      });
+    }
+    return{
+      triggerNegative,
+      triggerOngoing
+    }
+  },
   data() {
     return {
       name: '',
@@ -73,11 +98,83 @@ export default {
       password: '',
       passwordCheck: '',
       nickname: '',
-
+      errorMessage: '',
     }
   },
   methods: {
+    validateEmail() {
+      if (!this.isValidEmail(this.email)) {
+        this.errorMessage = '유효한 이메일 주소를 입력해주세요.';
+      } else {
+        this.errorMessage = '';
+      }
+    },
+    validateName() {
+      if (!this.name) {
+        this.errorMessage = '이름을 입력해주세요.';
+      } else {
+        this.errorMessage = '';
+      }
+    },
+    validateNickname() {
+      if (!this.nickname) {
+        this.errorMessage = '닉네임을 입력해주세요.';
+      } else {
+        this.errorMessage = '';
+      }
+    },
+    validatePassword() {
+      if (!this.isValidPassword(this.password)) {
+        this.errorMessage = '비밀번호는 최소 8자 이상, 15자 이하의 숫자를 입력하세요. 알파벳 대소문자(a~z, A~Z), 숫자(0~9)가 혼합되어야 합니다.';
+      } else {
+        this.errorMessage = '';
+      }
+    },
+    validatePasswordCheck() {
+      if (this.password !== this.passwordCheck) {
+        this.errorMessage = '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
+      } else {
+        this.errorMessage = '';
+      }
+    },
+    isValidPassword(password) {
+      return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/.test(password);
+    },
+    isValidEmail(email) {
+      return /.+@.+\..+/.test(email);
+    }
+    ,
     register() {
+
+      if (!this.name || !this.email || !this.password || !this.nickname) {
+        this.errorMessage = '모든 필드를 입력해주세요.';
+        this.triggerNegative(this.errorMessage);
+        return;
+      }
+
+      // 이메일 형식이 올바르지 않을 경우
+      if (!this.isValidEmail(this.email)) {
+        this.errorMessage = '유효한 이메일 주소를 입력해주세요.';
+        this.triggerNegative(this.errorMessage);
+        return;
+      }
+
+      // 비밀번호가 유효하지 않을 경우
+      if (!this.isValidPassword(this.password)) {
+        this.errorMessage = '비밀번호는 최소 8자 이상, 15자 이하의 숫자를 입력하세요. 알파벳 대소문자(a~z, A~Z), 숫자(0~9)가 혼합되어야 합니다.';
+        this.triggerNegative(this.errorMessage);
+        return;
+      }
+
+      // 비밀번호와 비밀번호 확인이 일치하지 않을 경우
+      if (this.password !== this.passwordCheck) {
+        this.errorMessage = '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
+        this.triggerNegative(this.errorMessage);
+        return;
+      }
+
+      this.errorMessage ='';
+
       const formData = {
         name: this.name,
         email: this.email,
@@ -87,22 +184,16 @@ export default {
       };
 
       axios.post('http://localhost:8080/api/member/create', formData)
-        .then(response => {
-          console.log(response.data);
-          // 회원가입 성공 시 처리할 내용 추가
-        })
-        .catch(error => {
-          console.error(error);
-          // 회원가입 실패 시 처리할 내용 추가
-        });
-    }
+      .then(response => {
+        this.triggerOngoing();
+        this.$router.push('/');
+      })
+      .catch(error => {
+        this.triggerNegative(error.response.data.message);
+      });
+    },
   }
   ,
-  setup() {
-
-
-    return {}
-  }
 }
 </script>
 
