79ac2ed131b5d78d59c98038637872ece7e99f13
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-02-20 19:09:48 +0900

    Jgn/register (#3)
    * feat : 프론트 axios 연동
    
    * feat: 회원가입
    
    외부 플러그인 notify 를 사용해 경고 창 띄우기
    
    * save: 이메일 회원가입 완료
    
    * feat: oauth로 인한 로그인 및 회원가입 연동
    
    1. 서버 8080 get으로 진입
    2. 서버에서 OAuthRedirect.vue로 쿠키에 jwt를 담아서 리다이렉트
    3. OAuthRedirect에서 쿠키에 있는 jwt를 로컬스토리지에 담고 쿠키 삭제
    4. 홈화면으로 리다이렉트
    
    * feat: 회원가입 이메일 인증
    
    예외처리 완료
    
    * feat: 일반이메일 로그인
    
    * feat: 로그인 상태 유지
    
    * feat: 로그인 상태 유지

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -91,7 +91,7 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
     framework: {
-      config: {},
+
       // iconSet: 'fontawesome-v6',
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -104,7 +104,13 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Notify'],
+
+      config: {
+        notify: {
+
+        }
+      }
     },
 
     // animations: 'all', // --- includes all animations
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,78 @@
 // 사용자 인증 및 권한 부여와 관련된 작업을 처리하는 파일로, 사용자 로그인, 로그아웃, 회원가입 등의 작업을 수행하고 사용자의 인증 상태를 관리
+import axios from 'axios';
+
+import { triggerOngoing, triggerNegative } from 'src/utils/notification';
+export const FountURL = 'http://localhost:8081';
+export const BackURL = 'http://localhost:8080';
+
+export function RegisterApi(formData, $q, router) {
+  axios.post(BackURL+'/api/member/create', formData)
+    .then(response => {
+      triggerOngoing('회원가입 했습니다. 로그인해주세요.', $q);
+      router.push('/');
+    })
+    .catch(error => {
+      triggerNegative(error.response.data.message, $q);
+    });
+}
+
+export function githubApi(){
+  location.href=BackURL+'/oauth2/authorization/github';
+}
+export function googleApi(){
+  location.href=BackURL+'/oauth2/authorization/google';
+}
+
+export function emailLogin(formData, $q, router){
+  axios.post(BackURL+'/api/member/login', formData)
+    .then(response => {
+      triggerOngoing('로그인되었습니다.', $q);
+      localStorage.setItem('accessToken',response.data.result)
+      location.reload();
+      router.push('/');
+    })
+    .catch(error => {
+      console.log(error);
+      triggerNegative(error.response.data.message, $q);
+    });
+}
+
+export function checkEmail(formData, $q)   {
+  axios.post(BackURL+'/api/member/emailAuthentication', formData)
+    .then(response => {
+      triggerOngoing('해당 메일로 인증번호를 발송했습니다.', $q);
+    })
+    .catch(error => {
+      triggerNegative(error.response.data.message, $q);
+    });
+}
+
+export function checkCode(formData, $q) {
+  return new Promise((resolve, reject) => {
+    axios.post(BackURL+'/api/member/emailCheck', formData)
+      .then(response => {
+        triggerOngoing(response.data.message, $q);
+        resolve(true); // 성공 시 true 반환
+      })
+      .catch(error => {
+        triggerNegative(error.response.data.message, $q);
+        resolve(false); // 실패 시 false 반환
+      });
+  });
+}
+
+export function Logout(router){
+  localStorage.removeItem("accessToken");
+  location.reload();
+  // return new Promise((resolve, reject) => {
+  //   axios.post(BackURL+'/api/member/logout')
+  //     .then(response => {
+  //       triggerOngoing(response.data.message, $q);
+  //       resolve(true); // 성공 시 true 반환
+  //     })
+  //     .catch(error => {
+  //       triggerNegative(error.response.data.message, $q);
+  //       resolve(false); // 실패 시 false 반환
+  //     });
+  // });
+}
