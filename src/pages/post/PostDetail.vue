c73cbb3cd6e06d1ef9998c9e772be59494f4bee3
cherishjh <139551676+cherishjh@users.noreply.github.com>
2024-02-22 19:40:18 +0900

    post detail

--- src/pages/post/PostDetail.vue
+++ src/pages/post/PostDetail.vue
@@ -1,76 +1,98 @@
 <template>
+  <q-page class="my-page row container">
+    <!-- 사이드 바 -->
+    <AppSidebar></AppSidebar>
+
+    <q-item-section>
+
   <div class="q-ma-md">
     <q-breadcrumbs class="text-orange" active-color="secondary" >
       <q-breadcrumbs-el :label="`${postDetail.spaceType} ${postDetail.spaceName}`" icon="widgets" />
-      <!-- <q-breadcrumbs-el :label="`${postDetail.title}`" /> -->
     </q-breadcrumbs>
+ 
     <div class="row no-wrap items-center q-mt-md q-pa-sm bg-grey-9 text-white rounded-borders">
-      <div class="nickname">WRITER: {{ postDetail.nickname }}</div>
-      
-      <q-space style="margin-right: 100px" />
-  
-      <div class="status">
-          <div>{{ postDetail.postStatus }}</div>
-          <!-- <div>Created: {{ postDetail.created_at }}</div>
-          <div>Updated: {{ postDetail.updated_at }}</div> -->
-      </div>
+      <div> WRITER: {{ postDetail.nickname || 'N/A' }}</div>
       <q-space />
-      <q-btn color="primary" label="Menu">
-        <q-menu>
-          <q-list style="min-width: 100px">
-            <q-item clickable v-close-popup>
-              <q-item-section>New tab</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>New incognito tab</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable v-close-popup>
-              <q-item-section>Recent tabs</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>History</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>Downloads</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable v-close-popup>
-              <q-item-section>Settings</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable v-close-popup>
-              <q-item-section>Help &amp; Feedback</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-  </div>
-
+        <div class="status-item"> STATUS: {{ postDetail.postStatus }}</div>
+        <div class="status-item"> HEARTS: {{ postDetail.postHearts }}</div>
+    </div>
     <q-scroll-area
       :thumb-style="thumbStyle"
       :bar-style="barStyle"
       style="height: 400px; max-width: 2000px; background-color: white;"
     > 
-    <div class="container">
-      <p> Created At: {{ postDetail.created_at }}</p> 
-      <p> Updated At: {{ postDetail.updated_at }}</p>
+    <div >
+      <p class="date-item">  Created At: {{ formatDateTime(postDetail.created_at) }}</p> 
+      <p class="date-item">  Updated At: {{ formatDateTime(postDetail.updated_at) }}</p>
     </div>
     <div v-if="postDetail" class="q-pa-xs" >
       <h3>{{ postDetail.title }}</h3>
-      <img :src="getImageUrl(postDetail.thumbnail)"  alt="Thumbnail" style="max-width: 100%;" />
-      <p>{{ postDetail.contents }}</p>
+      <q-card flat bordered class="q-pa-md q-ma-lg">
+        <div v-html="renderedHtml"></div>
+      </q-card>
     </div>
     </q-scroll-area>
-  </div>
+    
+    <div class="row no-wrap items-center q-mt-md q-pa-sm bg-grey-9 text-white rounded-borders">
+      <div>COMMENTS: {{ postDetail.commentCounts }}</div>
+    </div>
+    
+    <q-scroll-area
+      :thumb-style="thumbStyle"
+      :bar-style="barStyle"
+      style="height: 400px; max-width: 2000px; background-color: white;"
+    > 
+    <div v-for="comment in postComments" :key="comment.id">
+      <q-card flat bordered class="my-card" :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-2'">
+        <q-card-section>
+          <div class="text-h7"> 작성자: {{ comment.nickname }}</div>
+        </q-card-section>
+        <q-card-section>
+          <div class="text-h6"> {{ comment.contents }}</div>
+        </q-card-section>
+<!--         
+        <div class="col-auto">
+              <q-btn color="grey-7" round flat icon="more_vert">
+                <q-menu cover auto-close>
+                  <q-list>
+                    <q-item clickable>
+                      <q-item-section>삭제</q-item-section>
+                    </q-item>
+                    <q-item clickable>
+                      <q-item-section>Share</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
+            </div>
+          </div> -->
+
+          <q-separator />
+    
+          <q-card-actions>
+            <q-btn flat>답글</q-btn>
+            <q-btn flat>좋아요{{ comment.commentHearts }}</q-btn>
+          </q-card-actions>
+        </q-card>
+    </div>
+    </q-scroll-area>
+
+</div>
+<q-space />
+
+</q-item-section>
+</q-page>
 </template>
 
 <script>
-import axios from "axios";
 import {axiosInstance} from "boot/axios";
+import AppSidebar from "src/components/layout/AppSidebar.vue";
+
+import moment from 'moment';
 const BASE_URL = "http://localhost:8080"
 
 export default {
+  components: {AppSidebar},
   data () {
     return {
       thumbStyle: {
@@ -88,35 +110,58 @@ export default {
         width: '9px',
         opacity: 0.2
       },
-
-      postDetail: {}
+       // 서버에서 받아온 게시글 데이터
+       postDetail: { },
+       postComments: []
     };
   },
   methods:{
     async getPostDetail() {
       try {
-        console.log(TOKEN)
-        const response = await axiosInstance.get(`${BASE_URL}/api/post/detail/14`);
+        const response = await axiosInstance.get(`${BASE_URL}/api/post/detail/6`);
         this.postDetail = response.data.result
         console.log(this.postDetail);
       } catch (e) {
         console.log(e + "게시글을 불러오지 못했습니다.");
       }
     },
-    getImageUrl(thumbnailPath) {
-    // 정적 리소스 폴더의 경로와 이미지 파일명을 조합하여 URL 생성
-    return `/src/main/resources/static/images/${thumbnailPath}`;
-  }
+    formatDateTime(dateTime) {
+      return moment(dateTime).format('YYYY-MM-DD HH:mm:ss');
+    },
+    async getPostComments() {
+      try {
+        const response = await axiosInstance.get(`${BASE_URL}/api/comment/list/6`);
+        this.postComments = response.data.result
+        console.log(this.postComments);
+      } catch (e) {
+        console.log(e + "댓글을 불러오지 못했습니다.");
+      }
+    },
+
+   
   },
   created(){
     this.getPostDetail()
-    this.getImageUrl()
+    this.getPostComments()
   },
+  computed: {
+    renderedHtml() {
+      return this.postDetail.contents;
+    }
+  },
+
 }
 </script>
 <style>
-  .container {
-    text-align: right;
+  .date-item {
+    margin-bottom: 1px; /* 원하는 간격으로 조절 */
   }
+  .status {
+    display: flex;
+    flex-direction: row; /* 가로로 정렬 */
+  }
+  .status-item {
+    margin-right: 10px; /* 원하는 간격으로 조절 */
+  }
+ 
 </style>
-
