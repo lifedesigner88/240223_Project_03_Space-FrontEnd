efd657344509b49d92587fff79637ffee997a4e3
SejongPark <123573918+lifedesigner88@users.noreply.github.com>
2024-03-02 00:28:10 +0900

    Psj/my page 이름, 닉네임, 프로필 수정 (#12)
    * Psj/mypage 마이페이지 수정기능
      - 마이페이지에 수정을 위한 인풋박스 넣음.
    
    * Psj/mypage 이름과 닉네임 수정
    
     * 이름과 닉네임 수정
       - 마이페이지에 수정을 위한 인풋박스 넣음.
       - 변동사항 없으면 수정요청 불가
    
    * Psj/mypage 이름과 닉네임 프로필 사진 수정 (완성)
    
     * 이름과 닉네임 수정
       - 마이페이지에 수정을 위한 인풋박스 넣음.
       - 변동사항 없으면 수정요청 불가
    
     * 프로필 사진 변경 기능
       - 퀘이사 q-file 버그 발생
       - 기본 input 파일로 변경
       - 수업시간에 배운 방식으로 변경
       - 프로필 사진 업로드를 위해 백앤드 시큐리티 변경
        "/api/member/profile/image/**", 허용
       - 이메일 경로를 가지고 이미지 불러옴
         `${BASE_URL}/api/member/profile/image/${email}`
       - 수정시 화면 리로드 시켜서 사용자 경험 증대

--- src/pages/main/MyPage.vue
+++ src/pages/main/MyPage.vue
@@ -1,35 +1,38 @@
 <template>
-
-
   <q-page>
-    <q-form @submit="onSubmit" class="q-gutter-md">
-      <q-input outlined v-model="name" label="Name"/>
-      <q-input outlined v-model="email" label="Email"/>
-      <!-- 필요한 만큼의 입력 필드를 추가합니다 -->
-      <q-btn label="Submit" type="submit" color="primary"/>
-    </q-form>
-
     <q-item-section class="for_flex">
       <div class="container__in">
         <div class="myPage-header">
           <span>My Info</span>
-          <q-card-actions>
-            <q-btn class="btn__style" label="수정" color="green-5"/>
+          <q-card-actions class="botton__box">
+            <q-btn class="btn__style" label="수정" color="green-5" @click="patchMyInfo"/>
             <q-btn class="btn__style" label="탈퇴" color="red-5" @click="deleteMember"/>
           </q-card-actions>
         </div>
         <table class="table">
           <tr>
             <th>Profile :</th>
-            <td><img class="profile__box" :src="myInfo.profile" alt=""/></td>
+            <td>
+              <img v-if="myInfo.profile && myInfo.profile.startsWith('http')" class="profile__box" :src="myInfo.profile"
+                   alt=""/>
+              <img v-else class="profile__box" :src="getProfileImage(myInfo.email)" alt=""/>
+              <input type="file" ref="fileInput" @change="updateProfile" style="display: none"/>
+              <q-btn class="btn__style profile__btn" label="사진수정" color="blue-5" @click="promptFileUpload"/>
+            </td>
           </tr>
           <tr>
             <th>Name :</th>
-            <td>{{ myInfo.name }}</td>
+            <td>
+              <q-input class="input__box" bg-color="orange-3" outlined label-color="red-4" :label="myInfo.name"
+                       v-model="name"/>
+            </td>
           </tr>
           <tr>
             <th>Nick Name :</th>
-            <td>{{ myInfo.nickname }}</td>
+            <td>
+              <q-input class="input__box" bg-color="orange-3" outlined label-color="red-4" :label="myInfo.nickname"
+                       v-model="nickname"/>
+            </td>
           </tr>
           <tr>
             <th>Email :</th>
@@ -56,7 +59,6 @@ import {axiosInstance} from "boot/axios";
 import {Logout} from "src/services/authService";
 
 const BASE_URL = "http://localhost:8080"
-
 export default {
   components: {AppSidebar},
   setup() {
@@ -66,23 +68,68 @@ export default {
   data() {
     return {
       myInfo: {},
+      name: "",
+      nickname: "",
+      profile: null,
     }
   },
 
   methods: {
+
+    getProfileImage(email){
+      return `${BASE_URL}/api/member/profile/image/${email}`
+    },
+
+    promptFileUpload() {
+      this.$refs.fileInput.click();
+    },
+
+    async updateProfile(event) {
+      this.profile = event.target.files[0];
+      const formData = new FormData();
+      formData.append('profile', this.profile)
+
+      if (confirm("등록하신 사진으로 즉시 프로필이 변경됩니다. 변경하시겠습니까?"))
+      try {
+        await axiosInstance.put(`${BASE_URL}/api/member/profile`, formData)
+        window.location.reload();
+      } catch (e) {
+        console.log(e + "프로필 수정 실패");
+      }
+    },
+
     async loadMyInfo() {
       try {
         const response = await axiosInstance.get(`${BASE_URL}/api/member/mypage`);
         this.myInfo = response.data.result
+        this.name = this.myInfo.name;
+        this.nickname = this.myInfo.nickname;
       } catch (e) {
         console.log(e + "모든 스페이스 가져오기 실패");
       }
     },
+
+    async patchMyInfo() {
+      if (this.myInfo.name === this.name && this.myInfo.nickname === this.nickname)
+        alert("이름과 닉네임의 변경사항이 없습니다.")
+      else if (confirm("입력하신 정보로 수정하시겠습니까?"))
+        try {
+          const response = await axiosInstance.patch(`${BASE_URL}/api/member/patch`, {
+            name: this.name,
+            nickname: this.nickname
+          })
+          alert("수정완료")
+          await this.loadMyInfo();
+        } catch (e) {
+          console.log(e + "회원정보 수정 실패")
+        }
+    },
+
+
     async deleteMember() {
       if (confirm("모든 활동이 삭제됩니다 동의하십니까?"))
         try {
-          const response = await axiosInstance.get(`${BASE_URL}/api/member/delete`)
-          console.log(response.data.result)
+          await axiosInstance.delete(`${BASE_URL}/api/member/delete`)
           Logout(this.$q)
         } catch (e) {
           console.log(e + "회원 정보 삭제 실패")
@@ -91,7 +138,7 @@ export default {
   },
   created() {
     this.loadMyInfo();
-  }
+  },
 }
 </script>
 
@@ -101,7 +148,6 @@ export default {
   font-size: 20px;
   padding: 5px 20px 0 20px;
   font-weight: bold;
-
 }
 
 .container__in {
@@ -121,8 +167,9 @@ export default {
   //background-color: gray;
   display: flex;
   justify-content: space-around;
-  font-size: 3vw;
-  margin-top: 10vw;
+  font-size: 4vw;
+  margin-top: 50vh;
+  margin-bottom: 20px;
 }
 
 .table {
@@ -136,10 +183,12 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  //background-color: gray;
+
 }
 
 th {
-  padding: 10px;
+  padding: 15px;
 }
 
 td {
@@ -150,5 +199,23 @@ td {
   border-radius: 10px;
   width: 288px;
   height: 288px;
+}
+
+.input__box {
+  font-weight: bold;
+  font-size: 28px;
+  width: 100%;
+  color: #98dad1;
+}
+
+.profile__btn {
+  margin-left: 20px;
+  margin-bottom: 40px;
+}
+
+.botton__box {
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
